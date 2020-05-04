f7470e22f8fc6093e11cae9e099c707e4ba69a49
Tim McHugh <tim@startwithwhy.com>
2020-05-03 21:54:39 -0400

    Add Netlify Identity to index

--- pages/index.vue
+++ pages/index.vue
@@ -2,23 +2,11 @@
   <div class="container">
     <div>
       <logo />
-      <h1 class="title">
-        my-portfolio
-      </h1>
-      <h2 class="subtitle">
-        My exquisite Nuxt.js project
-      </h2>
+      <h1 class="title">my-portfolio</h1>
+      <h2 class="subtitle">My exquisite Nuxt.js project</h2>
       <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
       </div>
     </div>
   </div>
@@ -30,6 +18,13 @@ import Logo from '~/components/Logo.vue'
 export default {
   components: {
     Logo
+  },
+  head() {
+    return {
+      script: [
+        { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }
+      ]
+    }
   }
 }
 </script>
