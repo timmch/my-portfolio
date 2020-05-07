3da4e5122ccce74ebada205da7bb376fb914320d
Tim McHugh <tim@startwithwhy.com>
2020-05-07 10:53:39 -0400

    Lighlty style pages

--- pages/index.vue
+++ pages/index.vue
@@ -4,30 +4,41 @@
       <div class="max-w-lg text-center m-auto">
         <div>
           <img
-            class="h-56 rounded-full border-gray-100 border-4 m-auto shadow-lg"
+            class="h-40 md:h-56 rounded-full border-gray-100 border-4 m-auto shadow-lg"
             src="/img/Tim_Profile.jpg"
             alt="Tim Profile"
           />
 
-          <h1 class="font-bold text-4xl">My Portfolio</h1>
-          <h2>My exquisite Nuxt.js project</h2>
+          <h1 class="font-bold text-4xl text-gray-100 leading-tight mt-6">
+            Tim McHugh
+          </h1>
+          <h2 class="text-gray-100">My professional portfolio</h2>
         </div>
       </div>
     </div>
-    <div class="p-32 text-center">
-      <h4 class="text-lg text-blue-500 uppercase font-black">Portfolio</h4>
-      <h2 class="text-4xl">Recent Projects</h2>
-      <div class="grid grid-cols-2 text-left">
-        <div class="bg-red-500 p-16">
-          <h5 class="font-bold text-2xl">My favorite project</h5>
-          <p>Working with brilliant people!</p>
+    <div class="py-16 px-4 md:p-32 text-center">
+      <h4 class="text-lg text-blue-500 uppercase font-black leading-tight">
+        Portfolio
+      </h4>
+      <h2 class="text-4xl leading-tight">Recent Projects</h2>
+      <div class="grid md:grid-cols-2 text-left mt-6">
+        <div class="bg-red-500 py-8 px-12 md:p-16">
+          <h5 class="font-bold text-2xl leading-tight">My favorite project</h5>
+          <p class="mt-2">Working with brilliant people!</p>
         </div>
-        <div class="bg-yellow-500 p-16">
-          <h5 class="font-bold text-2xl">
+        <div class="bg-teal-500 py-8 px-12 md:p-16">
+          <h5 class="font-bold text-2xl leading-tight">
             The project that taught me the most
           </h5>
-          <p>Where I learned more about myself than the project.</p>
+          <p class="mt-2">
+            Where I learned more about myself than the project.
+          </p>
         </div>
+      </div>
+      <div class="mt-8">
+        <nuxt-link to="/projects" class="underline text-blue-500">
+          See all projects
+        </nuxt-link>
       </div>
     </div>
   </div>
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,11 +1,34 @@
 <template>
-  <div>
-    <div v-for="project in projects" :key="project.slug">
-      <nuxt-link
-        :to="{ name: 'projects-project', params: { project: project.slug } }"
-        prefetch
-        >{{ project.title }}</nuxt-link
+  <div class="px-8 py-16">
+    <div class="flex">
+      <div
+        v-for="tag in tags"
+        :key="tag.slug"
+        class="flex flex-col justify-center p-2 w-auto"
+        :style="{ background: tag.color }"
       >
+        <fa :icon="['fab', tag.icon]" class="fa--tag h-8" />
+
+        <h5 class="text-center">
+          {{ tag.title }}
+        </h5>
+      </div>
+    </div>
+    <div class="mt-6">
+      <div v-for="project in projects" :key="project.slug">
+        <nuxt-link
+          :to="{ name: 'projects-project', params: { project: project.slug } }"
+          prefetch
+        >
+          <div class=" border-gray-300 border-1 rounded shadow">
+            <img :src="project.thumbnail" alt="" />
+            <div class="p-4">
+              <h2 class="text-xl">{{ project.title }}</h2>
+              <p>{{ project.description }}</p>
+            </div>
+          </div>
+        </nuxt-link>
+      </div>
     </div>
   </div>
 </template>
@@ -15,9 +38,16 @@ export default {
   computed: {
     projects() {
       return this.$store.state.projects
+    },
+    tags() {
+      return this.$store.state.tags
     }
   }
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.fa--tag {
+  width: 100%;
+}
+</style>
