d8cb7c50e733e5f214aff3cea0407794ee42287b
Tim McHugh <tim@startwithwhy.com>
2020-05-11 17:38:05 -0400

    Add text module

---
+++ components/TextMessage.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <typing-indicator v-show="!showMessage" />
+    <div
+      v-show="showMessage"
+      class="bg-indigo-500 text-white font-sans font-light p-2 w-56 text-message mt-2 md:w-7/12 shadow rounded-lg"
+    >
+      <div v-html="textContent"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import TypingIndicator from '@/components/TypingIndicator.vue'
+
+export default {
+  components: {
+    TypingIndicator
+  },
+  props: {
+    textContent: {
+      type: String,
+      default: ''
+    },
+    delayTime: {
+      type: Number,
+      default: 1000
+    }
+  },
+  data() {
+    return {
+      showMessage: false
+    }
+  },
+  created() {
+    setTimeout(() => {
+      this.showMessage = true
+    }, this.delayTime)
+  }
+}
+</script>
+
+<style scoped>
+.text-message {
+  position: relative;
+}
+
+.text-message:after {
+  content: '';
+  position: absolute;
+  left: 0;
+  top: 50%;
+  width: 0;
+  height: 0;
+  border: 10px solid transparent;
+  border-right-color: #6781e7;
+  border-left: 0;
+  border-bottom: 0;
+  margin-top: -5px;
+  margin-left: -10px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,58 @@
 <template>
   <div>
-    <div class="bg-blue-600 pt-24 pb-16">
-      <div class="max-w-lg text-center m-auto">
-        <div>
-          <img
-            class="h-40 md:h-56 rounded-full border-gray-100 border-4 m-auto shadow-lg"
-            src="/img/Tim_Profile.jpg"
-            alt="Tim Profile"
-          />
-
-          <h1 class="font-bold text-4xl text-gray-100 leading-tight mt-6">
-            Tim McHugh
-          </h1>
-          <h2 class="text-gray-100">My professional portfolio</h2>
-        </div>
+    <div
+      class="flex flex-col px-6 py-2 items-start h-64 justify-end my-4 mx-4 sm:mx-auto  max-w-lg border border-gray-300 rounded shadow-lg overflow-y-scroll sm:w-full"
+    >
+      <div class="text-center w-full">
+        <h5><strong>Today</strong> 11:38 AM</h5>
       </div>
+
+      <text-message
+        v-show="showFirstText"
+        text-content="Hi 👋<br />I'm Tim McHugh."
+        delay-time="1000"
+      />
+
+      <text-message
+        v-if="showSecondText"
+        text-content="Welcome to my website, I'm glad you are here!"
+        delay-time="1250"
+      />
+
+      <text-message
+        v-if="showThirdText"
+        text-content="Take a look around to learn more about me and my work."
+        delay-time="1500"
+      />
+
+      <div v-for="(gif, index) in gifs" :key="index">
+        <img v-if="gifCount > index" :src="gif" class="w-7/12 mt-1 rounded" />
+      </div>
+
+      <div
+        v-show="showTextOptions"
+        class="flex flex-wrap justify-end mt-2 w-full"
+      >
+        <a
+          href="#projects"
+          class="border border-gray-800 rounded-full px-4 py-1 mt-1 shadow cursor-pointer"
+        >
+          Jump to projects
+        </a>
+        <a
+          v-show="gifCount < 3"
+          class="border border-gray-800 rounded-full px-4 py-1 mt-1 ml-1 shadow cursor-pointer"
+          @click="requestGif"
+        >
+          Request a gif
+        </a>
+      </div>
+
+      <!-- Loader -->
+      <!-- Take a look around to learn more about me and my work -->
     </div>
-    <div class="py-16 px-4 md:p-32 text-center">
+
+    <div id="projects" class="py-16 px-4 md:p-32 text-center">
       <h4 class="text-lg text-blue-500 uppercase font-black leading-tight">
         Portfolio
       </h4>
@@ -45,7 +81,49 @@
 </template>
 
 <script>
+import TextMessage from '@/components/TextMessage.vue'
+
 export default {
+  components: {
+    TextMessage
+  },
+  data() {
+    return {
+      showFirstText: false,
+      showSecondText: false,
+      showThirdText: false,
+
+      gifCount: 0,
+
+      showTextOptions: false,
+      gifs: ['/img/Pirates.webp', '/img/Dumb.gif', '/img/Shia.gif']
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showFirstText = true
+    }, 250)
+
+    // +2000
+    setTimeout(() => {
+      this.showSecondText = true
+    }, 2250)
+
+    // +2500
+    setTimeout(() => {
+      this.showThirdText = true
+    }, 5000)
+
+    // +500
+    setTimeout(() => {
+      this.showTextOptions = true
+    }, 7000)
+  },
+  methods: {
+    requestGif() {
+      this.gifCount++
+    }
+  },
   head() {
     return {
       script: [
@@ -55,3 +133,5 @@ export default {
   }
 }
 </script>
+
+<style></style>
